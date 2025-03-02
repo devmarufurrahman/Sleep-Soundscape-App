4d5f5dbad8ca509ad078322de80d673de3ead3a7
devmarufurrahman <mdmarufur5682@gmail.com>
2025-03-02 14:16:22 +0600

    sounds model add

---
+++ lib/model/sound_item.dart
@@ -0,0 +1,11 @@
+class SoundItem {
+  final String name;
+  final String iconPath;
+  final bool isLocked;
+
+  SoundItem({
+    required this.name,
+    required this.iconPath,
+    required this.isLocked,
+  });
+}
